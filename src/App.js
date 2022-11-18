31f08e307a517e970292173012cbebd3c1ebf184
Omar Vilera <ometrix@DESK0MAR.localdomain>
2022-11-18 02:46:02 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '@aws-amplify/ui-react/styles.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
 
-function App() {
+function App({ isPassedToWithAuthenticator, signOut, user }) {
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
+        <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
